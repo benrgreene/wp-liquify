cca27cf87c1a19379ce3648289f1235980ddfce9
Ben Greene <benrgreene@gmail.com>
2019-03-30 22:16:54 -0700

    feature: add base templates and WP info
    Added base wp info to pass to liquid templates (WP objects & pagination)

--- functions.php
+++ functions.php
@@ -2,12 +2,65 @@
 
 // Get all the WP Object info to pass to liquid
 function getWPObject () {
-  return array();
+  global $wp_query;
+
+  $data  = array();
+  $posts = getWPPostObjects();
+
+  // Add the post object(s)
+  if (1 == count($posts)) {
+    $data['post']  = $posts[0];
+  } else {
+    $data['posts'] = $posts;
+  }
+
+  // get any pagination data
+  if (is_archive() || is_home()) {
+    $pageNumber                 = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $data['pagination']['type'] = get_queried_object()->name;
+    $data['pagination']['page'] = $pageNumber;
+    if (1 < $pageNumber) {
+      $data['pagination']['previous'] = get_previous_posts_page_link();
+    }
+    if ($pageNumber < $wp_query->max_num_pages) {
+      $data['pagination']['next'] = get_next_posts_page_link();
+    }
+  }
+
+  return $data;
 }
 
 // Get the page type that is displaying
 function getWPTemplateName () {
-  $template = 'templates/theme.liquid';
+  $template = 'templates/';
 
-  return $template;
+  if (is_archive() || is_home()) {
+    $template .= 'archive';
+  } else if (is_front_page()) {
+    $template .= 'home';
+  } else {
+    $template .= 'page';
+  }
+
+  return $template . '.liquid';
+}
+
+// Get the WP Post objects to be passed to liquid
+function getWPPostObjects () {
+  global $post;
+
+  $toReturn = array();
+  if ( have_posts() ) { 
+    while ( have_posts() ) {
+      the_post();
+      $toReturn[] = array(
+        'id'       => $post->ID,
+        'title'    => $post->post_title,
+        'content'  => $post->post_content,
+        'url'      => get_permalink(),
+      );
+    }
+  }
+
+  return $toReturn;
 }
